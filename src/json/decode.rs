ae1ba147e4cc2c9601e0655b5a35095d8eff9cb4
MwlLj <731025894@qq.com>
2020-03-01 22:42:56 +0800

    update

--- src/json/decode.rs
+++ src/json/decode.rs
@@ -1,7 +1,25 @@
+use std::collections::HashMap;
+
+struct Object(HashMap<String, Value>);
+
+impl Object {
+    fn insert(&mut self, k: String, v: Value) {
+        self.0.insert(k, v);
+    }
+    fn new() -> Self {
+        Self(HashMap::new())
+    }
+}
+
+struct Array(Vec<Value>);
+
 enum Value {
+    None,
     Str(String),
     Int32(i32),
-    Float32(f32)
+    Float32(f32),
+    Object(Object),
+    Array(Array)
 }
 
 enum Token {
@@ -14,14 +32,134 @@ enum TokenMode {
     DoubleQuota
 }
 
+enum ParseMode {
+    Normal,
+    Big,
+    Mid,
+    Quota
+}
+
+enum InnerMode {
+    Normal,
+    FindQuota,
+    Quota,
+    FindColon,
+    Colon,
+    FindComma,
+    ValueQuota,
+    Comma
+}
+
+struct Inner {
+    mode: InnerMode,
+    word: String,
+    key_tmp: String,
+    value: Value
+}
+
+impl Default for Inner {
+    fn default() -> Self {
+        Self {
+            mode: InnerMode::Normal,
+            word: String::new(),
+            key_tmp: String::new(),
+            value: Value::None
+        }
+    }
+}
+
+impl Inner {
+    fn clear(&mut self) {
+        self.mode = InnerMode::Normal;
+        self.word.clear();
+        self.key_tmp.clear();
+        self.value = Value::None;
+    }
+}
+
 pub struct Json {
 }
 
 impl Json {
     pub fn from_str(&self, s: &str) {
+        let chars = s.chars();
+        let mut parse_mode = ParseMode::Normal;
+        let mut inner_data = Inner::default();
+        let mut value = Value::None;
+        for c in chars {
+            self.object_parse(c, &mut parse_mode, &mut inner_data, &mut value);
+        }
     }
 }
 
+impl Json {
+    fn object_parse(&self, c: char, parse_mode: &mut ParseMode, inner_data: &mut Inner, value: &mut Value) {
+        match parse_mode {
+            ParseMode::Normal => {
+                match c {
+                    '{' => {
+                        inner_data.value = Value::Object(Object::new());
+                        *parse_mode = ParseMode::Big;
+                        inner_data.mode = InnerMode::FindQuota;
+                    },
+                    '[' => {
+                    },
+                    _ => {
+                    }
+                }
+            },
+            ParseMode::Big => {
+                if c == '}' {
+                }
+                match &inner_data.mode {
+                    InnerMode::FindQuota => {
+                        if c == '"' {
+                            inner_data.mode = InnerMode::Quota;
+                        } else {
+                        }
+                    },
+                    InnerMode::Quota => {
+                        if c == '"' {
+                            inner_data.key_tmp = inner_data.word.clone();
+                            inner_data.mode = InnerMode::FindColon;
+                            inner_data.word.clear();
+                        } else {
+                            inner_data.word.push(c);
+                        }
+                    },
+                    InnerMode::FindColon => {
+                        if c == ':' {
+                            inner_data.mode = InnerMode::Colon;
+                        } else {
+                        }
+                    },
+                    InnerMode::Colon => {
+                        if c == ',' {
+                        } else if c == '"' {
+                            inner_data.mode = InnerMode::ValueQuota;
+                        } else if c == '{' || c == '[' {
+                        } else {
+                        }
+                    },
+                    InnerMode::ValueQuota => {
+                        if c == '"' {
+                            inner_data.mode = InnerMode::Colon;
+                            inner_data.word.clear();
+                        } else {
+                            inner_data.word.push(c);
+                        }
+                    },
+                    _ => {
+                    }
+                }
+            },
+            _ => {
+            }
+        }
+    }
+}
+
+/*
 impl Json {
     fn token_extract(&self, s: &str) {
         let mut tokens: Vec<Token> = Vec::new();
@@ -67,3 +205,4 @@ impl Json {
         }
     }
 }
+*/
